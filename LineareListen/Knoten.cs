2228e8eb0591754d781aaed2eaa63f3ce875c7dc
Klaus <BitLCEntwicklerDortmund@web.de>
2023-03-10 14:37:04 +0100

    Add project files.

---
+++ LineareListen/Knoten.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineareListen {
+    internal class Knoten {
+        private int info;
+        private Knoten next;
+
+        public void SetInfo(int z) {
+            info = z;
+        }
+        public int GetInfo() {
+            return info;
+        }
+        public void SetNext(Knoten k) {
+            next = k;
+        }
+        public Knoten GetNext() {
+            return next;
+        }
+        // Konstruktoren
+        public Knoten() { }
+        public Knoten(int z) {
+            info = z;
+        }
+        public Knoten(int z, Knoten k) {
+            info = z;
+            next = k;
+        }
+    }
+}
---
+++ LineareListen/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LineareListen {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, Liste!");
+
+            Knoten first = new Knoten(10, null);
+            Knoten newK = new Knoten(12, null);
+            Knoten akt = first;
+            akt.SetNext(newK);
+            newK = new Knoten(13, null);
+            akt = first;
+            akt = akt.GetNext();
+            akt.SetNext(newK);
+
+            // Anzeigen
+
+            akt = first;
+            //Console.WriteLine(  akt.GetInfo());
+            //akt = akt.GetNext();
+            //Console.WriteLine(akt.GetInfo());
+            //akt = akt.GetNext();
+            //Console.WriteLine(akt.GetInfo());
+
+            while (akt.GetNext() != null) {
+                Console.WriteLine(akt.GetInfo());
+                akt = akt.GetNext();
+                if (akt.GetNext() == null) {
+                    Console.WriteLine(akt.GetInfo());
+                }
+            }
+
+            LineareListe ll = new LineareListe();
+            ll.Add("Mohamad");
+            ll.Add(123);
+            ll.RemoveFirst();
+
+            Console.Read();
+        }
+    }
+}
